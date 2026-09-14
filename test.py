8c6b0b10963faf3b13f1ef48aaf3ed7506cb60a5
TarunMishra-DE <tarunmishra@outlook.de>
2026-09-14 15:24:50 +0200

    app added

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
-# from backend import run_travel_agent
+from backend import run_travel_agent
 
-res1 = tavily_search("Best hotels in India")
+""" res1 = tavily_search("Best hotels in India")
 print(res1)
-print("\n")
+
 
 res2 = search_flights("Plan a 7 days India trip from DE")
-print(res2)
+print(res2) """
 
-""" user_input = input("Enter travel request: ")
+user_input = input("Enter travel request: ")
 
 response = run_travel_agent(
     user_input=user_input,
     thread_id="test_user"
 )
-"""
-# print("\nFINAL RESPONSE:\n")
-# print(response["answer"]) 
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"]) 
